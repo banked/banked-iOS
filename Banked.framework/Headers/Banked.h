591f45d304d330202b675b263202c2acc50f06f9
kristinaSl <kristina.sl.nenkova@gmail.com>
2020-06-09 12:24:00 +0100

    Initial commit

---
+++ Banked.framework/Headers/Banked.h
@@ -0,0 +1,19 @@
+//
+//  Banked.h
+//  Banked
+//
+//  Created by Kristina Borisova on 06/05/2020.
+//  Copyright © 2020 Kristina Borisova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Banked.
+FOUNDATION_EXPORT double BankedVersionNumber;
+
+//! Project version string for Banked.
+FOUNDATION_EXPORT const unsigned char BankedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Banked/PublicHeader.h>
+
+
